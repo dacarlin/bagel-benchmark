1e80e257b2504ea5eb1cca63041d7efad274f15d
Alex Carlin <carlin@ucdavis.edu>
2016-03-25 22:18:33 -0700

    Improved sub.sh

--- sub.sh
+++ sub.sh
@@ -3,11 +3,8 @@
 #SBATCH --output=logs/slurm-%A_%a.out
 #SBATCH --error=logs/slurm-%A_%a.err
 #SBATCH --job-name=bglbench
-#SBATCH -n 1                    # Number of cores
-#SBATCH -t 0-4:05              # D-HH:MM
-##SBATCH --mem-per-cpu 8000
-#SBATCH --mail-type=END         # Type of email notification- BEGIN,END,FAIL,ALL
-#SBATCH --mail-user=carlin@ucdavis.edu 
+#SBATCH -n 1
+#SBATCH -t 0-1:00
 
 MUT=$( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
 /share/work/rosetta/source/bin/rosetta_scripts.linuxgccrelease @flags $MUT 
