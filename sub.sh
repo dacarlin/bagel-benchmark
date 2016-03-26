79b8ffe8b8395eb433140ba42463de95a44a1152
Alex Carlin <carlin@ucdavis.edu>
2016-03-25 20:15:52 -0700

    Major updates
    + Got rid of second repack sphere
    + Switched back to MutateResidue mover
    + Cleaned up list generation
    + Improved sub.sh

--- sub.sh
+++ sub.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
-#SBATCH --job-name=bglb
-#SBATCH --output=log.txt 
+#!/bin/bash
+# 
+#SBATCH --output=logs/slurm-%A_%a.out
+#SBATCH --error=logs/slurm-%A_%a.err
+#SBATCH --job-name=bglbench
+#SBATCH -n 1                    # Number of cores
+#SBATCH -t 0-4:05              # D-HH:MM
+##SBATCH --mem-per-cpu 8000
+#SBATCH --mail-type=END         # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=carlin@ucdavis.edu 
 
 MUT=$( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
 /share/work/rosetta/source/bin/rosetta_scripts.linuxgccrelease @flags $MUT 
+
