8d62aa2fee666f9fee73d1d3213d0e2a19350a32
Alex Carlin <carlin@ucdavis.edu>
2016-05-21 03:03:17 -0700

    Reorganized, still cleaning up

---
+++ protocols/playground/sub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 
+#SBATCH --output=logs/slurm-%A_%a.out
+#SBATCH --error=logs/slurm-%A_%a.err
+#SBATCH --job-name=bglb 
+#SBATCH -n 1
+#SBATCH -t 0-1:00
+#SBATCH --mem-per-cpu 2GB
+#SBATCH -p gc128 
+#SBATCH --array=1-2799
+
+MUT=$( sed -n "$SLURM_ARRAY_TASK_ID p" err_list ) 
+/share/work/rosetta/source/bin/rosetta_scripts.linuxgccrelease @flags $MUT 
+
