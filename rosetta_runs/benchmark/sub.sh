a8be453da7a0fc359988b9c821e0a439cf4417f3
Alex Carlin <dacarlin@ucdavis.edu>
2016-06-15 17:49:56 -0700

    Changed sub.sh for new module style

--- rosetta_runs/benchmark/sub.sh
+++ rosetta_runs/benchmark/sub.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # 
+#SBATCH -n 1
+#SBATCH -t 0-1:00
 #SBATCH --output=logs/slurm-%A_%a.out
 #SBATCH --error=logs/slurm-%A_%a.err
 #SBATCH --job-name=bglb 
-#SBATCH -n 1
-#SBATCH -t 0-1:00
 #SBATCH --mem-per-cpu 2GB
-#SBATCH -p gc128 
-#SBATCH --array=1-20600
+##SBATCH --array=1-20600
+#SBATCH --array=1 
 
+module load rosetta 
 MUT=$( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
-/share/work/rosetta/source/bin/rosetta_scripts.linuxgccrelease @flags $MUT 
+rosetta_scripts.linuxgccrelease @flags $MUT 
 
