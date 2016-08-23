8f57c69cd21e322b49e08db001951076ac88f8e9
Alex Carlin <dacarlin@ucdavis.edu>
2016-08-23 14:48:02 -0700

    Cleaning up FoldX runs

--- foldx/cab_run/make_list.py
+++
@@ -1,7 +0,0 @@
-my_str = '--aminoacids={} --positions={}'
-
-with open( 'individual_list.txt' ) as fn: 
-  mutant_list = fn.read().split() 
-
-for x in mutant_list:
-  print my_str.format( x[-1], x ) 
